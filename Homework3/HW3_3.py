ea14a7b021e7be7129e335de8259021967054e05
Kiritokun0909 <91424542+Kiritokun0909@users.noreply.github.com>
2023-10-22 09:36:28 +0700

    Add files via upload

--- Homework3/HW3_3.py
+++ Homework3/HW3_3.py
@@ -6,35 +6,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-# input file path
-file_path = 'dataset/actontBin.bin'
-
-
-def show_image(row, col, pos, image, title):
-    plt.subplot(row, col, pos)
-    plt.imshow(image, cmap='gray')
-    plt.title(title)
-
-
-# Construct an array from data in binary file
+# Load image
 w, h = 256, 256
-arr = np.fromfile(open(file_path), dtype=np.uint8).reshape(w, h)
+arr = np.fromfile(open('dataset/actontBin.bin'), dtype=np.uint8).reshape(w, h) / 255.0
 
-image = cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)
-show_image(2, 2, 1, image, 'actontBin.bin')
+# show original image
+plt.subplot(2, 2, 1)
+plt.imshow(arr, cmap='gray')
+plt.title('actontBin.bin')
 
-# Build an image of the letter T
-imgT = np.zeros((30, 22))
-imgT[2:7, 2:20] = 255
-imgT[7:27, 9:13] = 255
-show_image(2, 2, 2, imgT, 'T letter')
+# Build template of the letter T
+TemplRows, TemplCols = 47, 15
+template = np.zeros((TemplRows, TemplCols)).astype(np.uint8)
+template[10:16, :] = 255
+template[16:37, 6:10] = 255
 
-for i in range(h):
-    for j in range(w):
-        
+# show template
+plt.subplot(2, 2, 2)
+plt.imshow(template, cmap='gray')
+plt.title('T letter')
 
 # Binary Template Matching algorithm
+J1 = np.zeros((w, h))
+for row in range(h - TemplRows):
+    for col in range(w - TemplCols):
+        cmp_area = arr[row: row + TemplRows, col: col + TemplCols]
 
+        # xor: 2 pixels have the same value is True, else is False
+        matching_template = np.logical_not(np.logical_xor(cmp_area, template))
 
+        # sum of number of True
+        J1[row, col] = np.sum(matching_template)
+
+# show result after algorithm
+plt.subplot(2, 2, 3)
+plt.imshow(J1, cmap='gray')
+plt.title('J1 = M2(i, j)')
+
+# show max True value can get in Template Matching
+print(np.max(J1))
+
+# thresholding J1 to J2
+_, J2 = cv2.threshold(src=J1, thresh=685, maxval=705, type=cv2.THRESH_BINARY)
+
+plt.subplot(2, 2, 4)
+plt.imshow(J2, cmap='gray')
+plt.title('J2')
 
 plt.show()
